283f301dc5294690e4ce092f2fdb5f9aa27b250b
Munadir Ahmed <munadir_ahmed@yahoo.com>
2015-06-18 21:55:23 -0400

    forgot to add the changes in the previous commits for run_analysis.R and README.md

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# tidyData
-This repository is for the coursera project for Getting and Cleaning data
+# Tidy Data for Coursera Project
+
+The dataset summarises the activity of a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. The tidy data set provides an average of multiple measurements made for each activity for each volunteer. It includes records of both the training and test data sets.
+
+A script (run_analysis.R) takes raw data available that was downloaded from the following link:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+The folder was unzipped and the placed the same folder as the script. This is important as the script usess relative paths to load the data into R. The data has not been added to the Github accound as it can be downloaded from the link above. Please refer to the README.txt file in that folder to get a detailed description of the different data files and the experimental setup.
+
+The script creates a folder called TidyDataSet (if it doesn't already exits). It uses the 'dplyr' and 'reshape2' library packages which are loaded. Users must install this package in order to be able to run this script properly.
+
+The measurement variable names and activity names are loaded into the R environment. The measurement variables of interest are subsetted from the total list, and the data for the training and test sets are loaded into R. These data sets are combined to form a larger data sets.
+
+Finally, a summary (average) of each set of measurements is provided for each subject across each activity that they perform. This is provided as a dataset for futuer analysis in the TidyDataSet folder.
+
+The file variableNames.txt in the TidyDataSet folder contains all the different variables for the measurements.
+
+For each record it is provided:
+======================================
+
+- A SubjectID to indicate the volunteer number
+- An ActivityID to indicate the activity the subject was doing when all the measurements were conducted.
+- Average on each measurement of interest for 66 different types of measurementes.
+
+The dataset includes the following files:
+=========================================
+I
+- 'README.md'
+
+- 'run_analysis.R': script to create the desired tidy data set from the raw data
+
+- 'UCI HAR Dataset': folder that contains all the data. This is the folder that has to be extracted from the link above.
+
+- 'TidyDataSet/tidyAveragedDataSet.txt': text file containing average values for 66 variables of interest. Each row summarises the average of measurements taken on each subject doing one of six activities.
+
+Notes: 
+======
+- Features are normalized and bounded within [-1,1].
+- Each column (apart from SubjectID and ActivityID) represents a measurement of interest.
--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,79 @@
-setwd("~/Documents/Personal/MOOCs/Coursera/Data Science/Getting and Cleaning Data/Project/tidyData/")
+##Set the working directory to folder containing run_analysis and the UCI HAR Dataset folder
 
+#Commenting out for end user
+#setwd("~/Documents/Personal/MOOCs/Coursera/Data Science/Getting and Cleaning Data/Project/tidyData/")
+
+## Load required libraries
 library(dplyr)
-# Create a new directory to store the tidy dataset if it doesn't already exits
+library(reshape2)
+
+## Create a new directory to store the tidy dataset if it doesn't already exits
 if(!file.exists("TidyDataSet"))
 {
     dir.create("TidyDataSet")
 }
 
-## Derive columns of interest from datasets
+## Derive columns of interest from datasets and store activity labels
 
 columnIdDF<-read.table(file="./UCI HAR Dataset/features.txt")
+activityID<-read.table(file="./UCI HAR Dataset/activity_labels.txt")
+
+# Mutate dataset to add third column to store which indices are of interest. Set default value to NULL.
 
 columnIdDF<-mutate(columnIdDF,boolLoad = 'NULL')
 
-# Obtain the index number of variables that store the mean and the std of measurements
+# Obtain the index number of variables that store the mean and the std of measurements. 
 indexOfMean <-grep("-mean()" ,columnIdDF[,2],fixed=TRUE)
 indexOfStd <-grep("-std()" ,columnIdDF[,2],fixed=TRUE)
 
 # merge the columns into one variable:
 columnIndex <- c(indexOfMean,indexOfStd)
 
-columnIdDF[columnIndex,3]<-"double" # set the columns that need to be loaded to TRUE
+#Set the third column for indices of interest to the type double (or the data type)
+columnIdDF[columnIndex,3]<-"double" 
 
 ## Training Set
 
 # read all the different text files into R from the train folder:
 
-trainingSubectId <-read.table(file="./UCI HAR Dataset/train/subject_train.txt")
+trainingSubjectId <-read.table(file="./UCI HAR Dataset/train/subject_train.txt")
 trainingSetData <-read.table(file="./UCI HAR Dataset/train/X_train.txt",colClasses = columnIdDF[,3],col.names = columnIdDF[,2])
 trainingSetLabel <-read.table(file="./UCI HAR Dataset/train/y_train.txt")
 
+## Make training dataset with appropriate variable labels
+
+TrainingDataSet <- cbind(trainingSubjectId,trainingSetLabel,trainingSetData)
+
+colnames(TrainingDataSet)[c(1,2)]<-c("SubjectID","ActivityID")
+
 # Test Set
 
-testSubectId <-read.table(file="./UCI HAR Dataset/test/subject_test.txt")
+testSubjectId <-read.table(file="./UCI HAR Dataset/test/subject_test.txt")
 testSetData <-read.table(file="./UCI HAR Dataset/test/X_test.txt",colClasses = columnIdDF[,3],col.names = columnIdDF[,2])
 testSetLabel <-read.table(file="./UCI HAR Dataset/test/y_test.txt")
+
+## Make training dataset with appropriate variable labels
+
+TestDataSet <- cbind(testSubjectId,testSetLabel,testSetData)
+
+colnames(TestDataSet)[c(1,2)]<-c("SubjectID","ActivityID")
+
+## Convert the Activity into a factors variable and replace the number with the activity
+
+TrainingDataSet[,2]<-factor(TrainingDataSet[,2],labels = activityID[,2])
+TestDataSet[,2]<-factor(TestDataSet[,2],labels = activityID[,2])
+
+## Merge the TestDataSet and TrainingDataSet to create a complete dataset
+
+MergedData=rbind(TestDataSet,TrainingDataSet)
+
+## Store the mean of each variable grouped by each subject and each activity from the total data set
+AveragedDataSet<-MergedData %>% group_by(SubjectID,ActivityID) %>% summarise_each(funs(mean))
+
+# Rearrange the dataset with respect to the SubjectID
+AveragedDataSet<-arrange(AveragedDataSet,SubjectID)
+
+## Write this to a text file in the TidyDataSet folder
+
+write.table(AveragedDataSet,file="./TidyDataSet/tidyAveragedDataSet.txt",row.names = FALSE) 
+#write.table(colnames(AveragedDataSet),file="./TidyDataSet/variableNames.txt",row.names = FALSE) 
